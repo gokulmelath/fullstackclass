8847925d90e1a7e7198a18ff380acd18476f15dd
gokulmelath <gokulmeloth@gmail.com>
2018-11-14 13:51:29 +0530

    python3

--- python/Flaskpgms/app.py
+++ python/Flaskpgms/app.py
@@ -1,16 +1,32 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from data import Students
 app=Flask(__name__)
+
+getstudents = Students()
+
+@app.route('/students')
+def stud():
+    return render_template('studentlist.html',mystudentlist=getstudents)
 
 @app.route('/')
 def index():
-    return 'welcome to flask'
-@app.route('/home')
-def home():
-    return 'welcome to Home'
+    return render_template('home.html')
 @app.route('/about')
 def about():
-    return 'welcome to aboutpage'        
+    return render_template('about.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')   
+
+@app.route('/contactus')
+def contactus():
+    return render_template('contactus.html')       
+      
+@app.route('/send',methods=['GET','POST'])
+def send():
+    if(request.method=='POST'):
+        getname=request.form['name']
+        return render_template('result.html',newname=getname) 
 
 if(__name__=='__main__'):
     app.run(debug=True)
